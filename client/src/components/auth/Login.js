800522676b8469f9e03681631b837f84bd5c6d36
Katie Gracey <gracey.kd@gmail.com>
2021-03-22 16:14:08 +0100

    add cancel buttons to login/signup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,83 +3,114 @@ import { login } from '../../services/auth';
 import './Auth.css';
 
 export default class SignUp extends Component {
-
   state = {
     username: '',
     password: '',
-    message: ''
-  }
+    role: '',
+  };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value })
-  }
+    this.setState({ [name]: value });
+  };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
     const { username, password } = this.state;
-    login(username, password)
-    .then(data => {
+    login(username, password).then((data) => {
       if (data.message) {
-        this.setState({ 
+        this.setState({
           message: data.message,
           username: '',
-          password: ''
+          password: '',
         });
       } else {
         //put the user in the state of App.js
         this.props.setUser(data);
         this.props.history.push('/');
       }
+    });
+  };
+
+  cancel = (event) => {
+    this.setState({
+      role: ''
     })
+    this.props.history.push('/');
   }
 
   render() {
     return (
-      <div className="auth-container">
-        <div className="left">
-          <div className="header">
-            <h2 className="animation a1">Welcome Back</h2>
-            <h4 className="animation a2">Log in to your account using username and password</h4>
-          </div>
-          <div className="form">
-            <form onSubmit={ this.handleSubmit }>
-              <div className="form-field-box">
-                {/* <label htmlFor="username">Username: </label> */}
-                <input
-                  className="form-field animation a3"
-                  type="text"
-                  id="username"
-                  name="username"
-                  value={ this.state.username }
-                  onChange={ this.handleChange }
-                  placeholder="Username"
-                  required
-                />
+      <>
+        {this.state.role ? (
+          <div className="auth-container">
+            <div className="left">
+              <div className="header">
+                <h2 className="animation a1">Welcome Back</h2>
+                <h4 className="animation a2">
+                  {this.state.role.charAt(0).toUpperCase() + this.state.role.slice(1)} login
+                </h4>
               </div>
-              <div className="form-field-box">
-                {/* <label htmlFor="password">Password: </label> */}
-                <input
-                  className="form-field animation a4"
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={ this.state.password }
-                  onChange={ this.handleChange }
-                  placeholder="Password"
-                  required
-                />
+              <div className="form">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-field-box">
+                    {/* <label htmlFor="username">Username: </label> */}
+                    <input
+                      className="form-field animation a3"
+                      type="text"
+                      id="username"
+                      name="username"
+                      value={this.state.username}
+                      onChange={this.handleChange}
+                      placeholder="Username"
+                      required
+                    />
+                  </div>
+                  <div className="form-field-box">
+                    {/* <label htmlFor="password">Password: </label> */}
+                    <input
+                      className="form-field animation a4"
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+                  {this.state.message && <p>{this.state.message}</p>}
+                  <br />
+                  <button className="button-dark animation a6" type="submit">
+                    Log In
+                  </button>
+                  <button onClick={this.cancel}>
+                    Cancel
+                  </button>
+                </form>
               </div>
-              {this.state.message && (
-                <p>{ this.state.message }</p>
-              )}
-              <br/>
-              <button className="button-dark animation a6"type="submit">Log In</button>
-            </form>
+            </div>
+            <div className="right"></div>
           </div>
-        </div>
-        <div className="right"></div>
-      </div>
-    )
+        ) : (
+          <div>
+            <h1>Login with Classroom Economy as</h1>
+            <button name="role" onClick={this.handleChange} value="student">
+              Student
+            </button>
+            <button name="role" onClick={this.handleChange} value="teacher">
+              Teacher
+            </button>
+            <button
+              name="role"
+              onClick={this.handleChange}
+              value="school-leader"
+            >
+              School Leader
+            </button>
+          </div>
+        )}
+      </>
+    );
   }
 }
--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -22,121 +22,139 @@ export default class SignUp extends Component {
     event.preventDefault();
     const { username, password, firstName, lastName, email, role } = this.state;
     console.log('role', role, 'username', username);
-    signup(username, password, firstName, lastName, email, role).then((data) => {
-      if (data.message) {
-        this.setState({
-          message: data.message,
-          username: '',
-          password: '',
-          firstName: '',
-          lastName: '',
-          email: '', 
-        });
-      } else {
-        // console.log(this.props);
-        this.props.setUser(data);
-        this.props.history.push('/');
+    signup(username, password, firstName, lastName, email, role).then(
+      (data) => {
+        if (data.message) {
+          this.setState({
+            message: data.message,
+            username: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+          });
+        } else {
+          // console.log(this.props);
+          this.props.setUser(data);
+          this.props.history.push('/');
+        }
       }
-    });
+    );
   };
+
+  cancel = (event) => {
+    this.setState({
+      role: ''
+    })
+    this.props.history.push('/');
+  }
+
 
   render() {
     return (
       <>
-      {this.state.role ? (
-        <div className="auth-container">
-        <div className="left">
-          <div className="header">
-            <h2>Welcome</h2>
-            <h4>Sign up to get that Coinnn</h4>
+        {this.state.role ? (
+          <div className="auth-container">
+            <div className="left">
+              <div className="header">
+                <h2>Welcome</h2>
+                <h4>Sign up to get that Coinnn</h4>
+              </div>
+              <div className="form">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-field-box">
+                    {/* <label htmlFor="username">Username: </label> */}
+                    <input
+                      className="form-field"
+                      type="text"
+                      id="username"
+                      name="username"
+                      value={this.state.username}
+                      onChange={this.handleChange}
+                      placeholder="Username"
+                      required
+                    />
+                  </div>
+
+                  <div className="form-field-box">
+                    {/* <label htmlFor="password">Password: </label> */}
+                    <input
+                      className="form-field"
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+
+                  {this.state.message && <p>{this.state.message}</p>}
+
+                  <div className="form-field-box">
+                    {/* <label htmlFor="firstName">Name: </label> */}
+                    <input
+                      className="form-field"
+                      type="text"
+                      id="firstName"
+                      name="firstName"
+                      value={this.state.firstName}
+                      onChange={this.handleChange}
+                      placeholder="First Name"
+                    />
+                  </div>
+                  <div className="form-field-box">
+                    {/* <label htmlFor="lastName">Last Name: </label> */}
+                    <input
+                      className="form-field"
+                      type="text"
+                      id="lastName"
+                      name="lastName"
+                      value={this.state.lastName}
+                      onChange={this.handleChange}
+                      placeholder="Last Name"
+                    />
+                  </div>
+                  <div className="form-field-box">
+                    {/* <label htmlFor="email">Email: </label> */}
+                    <input
+                      className="form-field"
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      placeholder="Email"
+                    />
+                  </div>
+                  <br />
+                  <button type="submit">Sign Up</button>
+                  <button onClick={this.cancel}>Cancel</button>
+                </form>
+              </div>
+            </div>
+            <div className="right"></div>
           </div>
-          <div className="form">
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-field-box">
-                {/* <label htmlFor="username">Username: </label> */}
-                <input
-                  className="form-field"
-                  type="text"
-                  id="username"
-                  name="username"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                  placeholder="Username"
-                  required
-                />
-              </div>
-
-              <div className="form-field-box">
-                {/* <label htmlFor="password">Password: </label> */}
-                <input
-                  className="form-field"
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                  placeholder="Password"
-                  required
-                />
-              </div>
-
-              {this.state.message && <p>{this.state.message}</p>}
-
-              <div className="form-field-box">
-                {/* <label htmlFor="firstName">Name: </label> */}
-                <input
-                  className="form-field"
-                  type="text"
-                  id="firstName"
-                  name="firstName"
-                  value={this.state.firstName}
-                  onChange={this.handleChange}
-                  placeholder="First Name"
-                />
-              </div>
-              <div className="form-field-box">
-                {/* <label htmlFor="lastName">Last Name: </label> */}
-                <input
-                  className="form-field"
-                  type="text"
-                  id="lastName"
-                  name="lastName"
-                  value={this.state.lastName}
-                  onChange={this.handleChange}
-                  placeholder="Last Name"
-                />
-              </div>
-              <div className="form-field-box">
-                {/* <label htmlFor="email">Email: </label> */}
-                <input
-                  className="form-field"
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleChange}
-                  placeholder="Email"
-                />
-              </div>
-              <br />
-              <button type="submit">
-                Sign Up
-              </button>
-            </form>
+        ) : (
+          <div>
+            <h1>Sign up with Classroom Economy as</h1>
+            <button name="role" onClick={this.handleChange} value="student">
+              Student
+            </button>
+            <button name="role" onClick={this.handleChange} value="teacher">
+              Teacher
+            </button>
+            <button
+              name="role"
+              onClick={this.handleChange}
+              value="school-leader"
+            >
+              School Leader
+            </button>
           </div>
-        </div>
-        <div className="right"></div>
-      </div>
-      ) : (
-        <div>
-          <h1>Sign up with Classroom Economy as</h1>
-          <button name="role" onClick={this.handleChange} value="student">Student</button>
-          <button name="role" onClick={this.handleChange} value="teacher">Teacher</button>
-          <button name="role" onClick={this.handleChange} value="school-leader">School Leader</button>
-        </div>
-      )}
+        )}
       </>
-      
     );
   }
 }
