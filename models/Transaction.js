9caebbb8a7a25f8e53b27c3229f03bd526f56196
Katie Gracey <gracey.kd@gmail.com>
2021-03-25 17:41:42 +0100

    transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const transactionSchema = new Schema(
+  {
+    payor: {type: Schema.Types.ObjectId, ref: 'user'},
+    payee: {type: Schema.Types.ObjectId, ref: 'user'},
+    amount: {
+      type: Number,
+      required: true
+    },
+    operation: {
+      type: String,
+      enum: ['withdraw', 'deposit'],
+    },
+    category: {
+      type: String,
+      enum: ['rent', 'paycheck', 'fine', 'bonus', 'other']
+    },
+    description : String,
+    status: {
+      type: String,
+      enum: ['pending', 'complete']
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+module.exports = Transaction;
